e964a2f92f96f5bfabb0ec67e5451e410631b152
Mohammed Benmansour <benmansour.mohammed2002@gmail.com>
2024-06-30 13:05:07 +0200

    Added README.md + Added notebooks + Added tokenizer notebook + added tokenizer lib

---
+++ README.md
@@ -0,0 +1,18 @@
+# Horizen®
+Horizen® is an LLM project that aims to help me train on LLMs and transformers.
+
+
+## Getting started
+To start using/developping Horizen®, start by setting up a python virtual environment:
+```shell
+python3 -m venv .venv
+source .venv/bin/activate
+```
+
+And then install the dependencies:
+
+```shell
+pip install torch
+```
+
+> Note: in the notebooks, don't forget to choose your virtual environment.
--- scripts/lib/utils/compile.py
+++ scripts/lib/utils/compile.py
@@ -2,6 +2,7 @@ import os
 
 current_folder = os.path.dirname(os.path.realpath(__file__))
 
+
 def compileFolder(folder_name):
     """
     Compile all the files withing a folder into a class
@@ -12,19 +13,25 @@ def compileFolder(folder_name):
     folder_name: str
         The name of the folder to scrape (within assets folder)
     """
-    folder_path = os.path.abspath(os.path.join(current_folder,'..','..','assets',folder_name))
+    folder_path = os.path.abspath(
+        os.path.join(current_folder, "..", "..", "..", "assets", folder_name)
+    )
     content = []
     for file in os.listdir(folder_path):
         if file.endswith(".txt"):
-            with open(os.path.join(folder_path,file)) as f:
+            with open(os.path.join(folder_path, file)) as f:
                 content.append("".join(f.readlines()))
     return content
-            
 
 
-if __name__ == '__main__':
-    folder = input('Type in the folder name : ')
+if __name__ == "__main__":
+    folder = input("Type in the folder name : ")
     result = compileFolder(folder)
-    with open(os.path.abspath(os.path.join(current_folder,'..','..','logs',folder+'.txt')), "w") as f:
+    with open(
+        os.path.abspath(
+            os.path.join(current_folder, "..", "..", "logs", folder + ".txt")
+        ),
+        "w",
+    ) as f:
         f.write("\n\n".join(result))
         f.close()
